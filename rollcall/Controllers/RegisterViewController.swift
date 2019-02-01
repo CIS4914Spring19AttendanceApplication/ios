4419e399b43d4a5aaccd9f0a78e3f877671ac28c
samielynne <samieleboli@gmail.com>
2019-02-01 12:55:06 -0500

    basic layout of register screen

--- rollcall/Controllers/RegisterViewController.swift
+++ rollcall/Controllers/RegisterViewController.swift
@@ -8,19 +8,47 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
   
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     var emailPassedOver : String?
+    @IBOutlet weak var yearPicker: UIPickerView!
+    let yearArr = ["Freshman", "Sophomore", "Junior", "Senior", "Graduate"]
+    var year : String?
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return yearArr.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return yearArr[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        year = yearArr[row]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         emailField.placeholder = emailPassedOver
         emailField.isUserInteractionEnabled = false
+        
+        yearPicker.delegate = self;
+        yearPicker.dataSource = self;
     }
     
-
+    
+    @IBAction func register(_ sender: Any) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
